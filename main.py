de892c0fda577bdf27ba29734f2079150269ca01
weynars <lucasweynars@hotmail.com>
2023-03-03 22:02:04 +1100

    Added more comments to the code

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import plotly.figure_factory as ff
 st.set_page_config(layout="wide")
 st.title('Healthcare Attrition Propensity')
 
-
 filename = './data/healthcare_data.csv'
 
 x_cols = ['BusinessTravel',
@@ -76,11 +75,11 @@ label = 'Attrition Propensity'
 
 tab1, tab2 = st.tabs(["Predictions Dashboard", "Employee Risk"])
 
+# Predictions Dashboard
 with tab1:
+    # Filter Data 
     st.subheader('Filter')
     values = st.slider(label, min_value=0, max_value=100, value=(50,100), step=5, format="%d%%")
-
-    # Filter Data 
     
     # Apply filter
     cond1 = df[col] >= values[0]/100
@@ -108,44 +107,39 @@ with tab1:
     with col2:
         # Print table with top 100 at risk employees
         st.subheader('Employees at Risk')
-        st.write('Shows top 100 employees at risk of leaving within the selected attritiob propensity range filter.')
+        st.write('Shows top 100 employees at risk of leaving within the selected attrition propensity range filter.')
         # Center the dataframe
         with st.columns([1,3,1])[1]:
             st.dataframe(filtered_df[[label]].sort_values(label, ascending=False).head(100))
 
+# Employee Risk
 with tab2:
+    # Get list of employees to add to Selection Box
     employee_ids = df['EmployeeID'].unique()
     employee_id = st.selectbox('Select Employee ID', employee_ids)
 
+    # Filter data for selected employee
     cond3 = df['EmployeeID'] == employee_id
     employee_data = df[cond3]
     employee_data.set_index('EmployeeID', inplace=True)
     st.write(f'Employee **{employee_id}** has a propensity to leave of:', "**{:.2%}**".format(employee_data[col].iloc[0]))
 
 
-    # Show main contributors
+    # Extract model and preprocessor from model pipeline
     preprocessor = model_pipeline[0]
     model = model_pipeline[-1]
 
+    # Calculate feature impact
     x_t = preprocessor.transform(employee_data[x_cols])
     coefs = model.coef_[0]
-    intercept = model.intercept_
-    
-    # x_t[x_t == 0] -= 1
     feature_impact = x_t * coefs
-    abs_feature_impact = np.abs(feature_impact)
-
-    # score = np.dot(x_t, coefs) + intercept
-    # propensity = 1 - 1./(1.+np.exp(score))
     
-    # odds = np.exp(coefs)
-    
+    # Transform feature impact to pandas Dataframe
     feature_names = preprocessor.get_feature_names_out()
     feature_impact_df = pd.DataFrame(feature_impact.T,
         columns=['feature_impact'],
         index=feature_names,
     )
-    feature_impact_df['abs_feature_impact'] = abs_feature_impact.T
 
     st.header('Top attributes increasing employee\'s propensity to leave')
 
@@ -168,14 +162,17 @@ with tab2:
     with col1:
         st.subheader('Impact on propensity to leave')
         st.write('Positive values indicates the feature increases the attrition propensity and the negative values indicates a decrease in attrition propensity.')
+        
+        # Format data for plotly chart
         top_10_features = top_10_features.reset_index()
         top_10_features['Positive Impact'] = top_10_features['feature_impact'] > 0
         top_10_features.rename({'orig_features':'Attribute', 'feature_impact': 'Feature Impact'}, axis=1, inplace=True)
 
         # https://plotly.com/python/discrete-color/
         discrete_colors = [ px.colors.qualitative.Plotly[index] for index in [2,1] ]
-        fig = px.bar(top_10_features, x="Feature Impact", y="Attribute", color='Positive Impact', orientation='h', height=600,
-            color_discrete_sequence=discrete_colors)
+        fig = px.bar(top_10_features, x="Feature Impact", y="Attribute", color='Positive Impact', 
+            orientation='h', height=600, color_discrete_sequence=discrete_colors)
+        # Select order of the bars in the chart
         order_list = list(top_10_features['Attribute'].values)
         order_list.reverse()
         fig.update_yaxes(categoryorder='array', categoryarray= order_list)
